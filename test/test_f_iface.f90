b0a81c0ed77ad961b15af17d6f4d50842feae69d
Yannick de Jong <ydejong@ignitioncomputing.com>
2025-05-20 15:30:41 +0200

    Compile test

--- test/test_f_iface.f90
+++ test/test_f_iface.f90
@@ -14,38 +14,38 @@
 
 program test_sg_fortran_interface
 ! modules
-use singularity_eos_types
 use singularity_eos
 ! no implicit vars
 implicit none
 ! variable declaration
-integer                                   :: nmat, res, mat
-type(sg_eos_ary_t)                        :: eos
+type(sg_eos) :: eos
+real(kind=8), dimension(2) :: temps, rhos, sies
+logical, dimension(2) :: mask
 
-! set test parameters
-nmat = 5
+temps(1) = 1000.d0
+temps(2) = 2000.d0
 
-! allocate and initialize eos's
-res = init_sg_eos_f(nmat, eos)
-mat = 1
+rhos(1) = 0.1d0
+rhos(2) = 0.2d0
 
-res = init_sg_IdealGas_f(mat, eos, 1.4d0, 1.0d7)
+mask(1) = .false.
+mask(2) = .true.
 
-mat = mat + 1
-res = init_sg_Gruneisen_f(mat, eos, 394000.d0, 1.489d0, 0.d0, 0.d0, 2.02d0, 0.47d0,&
-                       8.93d0, 297.0d0, 1.0d6, 0.383d7)
+eos = init_IdealGas(1.4d0, 1.0d0)
 
-mat = mat + 1
-res = init_sg_JWL_f(mat, eos, 1.36177d13, 7.199d11, 6.2d0, 2.2d0, 0.5d0, 1.895d0, 1.0d7)
+sies(:) = 0.0d0
+sies = eos%InternalEnergyFromDensityTemperature(0.1d0, 3000.d0)
+! assert (sies == 3000)
 
-mat = mat + 1
-res = init_sg_DavisReactants_f(mat, eos, 1.890d0, 4.115d10, 1.0d6, 297.0d0, 1.8d0,&
-                            4.6d0, 0.34d0, 0.56d0,0.d0, 0.4265d0, 0.001074d10)
-mat = mat + 1
-res = init_sg_DavisProducts_f(mat, eos, 0.798311d0, 0.58d0, 1.35d0, 2.66182d0,&
-                           0.75419d0, 3.2d10, 0.001072d10)
+sies(:) = 0.0d0
+sies = eos%InternalEnergyFromDensityTemperature(rhos, temps)
+! assert sies == (1000, 2000)
 
-! cleanup
-res = finalize_sg_eos_f(nmat, eos)
+sies(:) = 0.0d0
+where (mask)
+    sies = eos%InternalEnergyFromDensityTemperature(rhos, temps)
+endwhere
+! assert sies == (0, 2000)
 
+call eos%Finalize()
 end program test_sg_fortran_interface
